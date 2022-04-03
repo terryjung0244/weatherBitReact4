c1e0339e21605605468f7882938ebe003db30319
terryjung0244 <terryjung0244@gmail.com>
2022-04-02 22:02:06 -0400

    Created list of cities

---
+++ cityList.js
@@ -0,0 +1,17 @@
+export const cityList = [
+  {
+    cityName: 'Montreal',
+    province: 'Quebec',
+    country: 'CA'
+  },
+  {
+    cityName: 'Los Angeles',
+    province: 'Califonia',
+    country: 'US'
+  },
+  {
+    cityName: 'Ansan',
+    province: 'Kyung-gi-do',
+    country: 'KOR'
+  }
+]
